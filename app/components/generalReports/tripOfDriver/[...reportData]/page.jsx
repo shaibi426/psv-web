569eba3294f0846596e91d60f6c35c423ff2bc18
m-atique <atiquenwl@gmail.com>
2023-08-11 15:13:19 +0500

    trip of driver API Coded and test report of trip

---
+++ app/components/generalReports/tripOfDriver/[...reportData]/page.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+import ReportTemp from '../../../'
+
+export default function TripOfDriver(props) {
+  const [data, setData] = useState([])
+  const reportprops = props.params.reportData[0].split("x");
+  const startDate = reportprops[0];
+  const endDate = reportprops[1];
+  const startTime = reportprops[2].split("%3A").join(":");
+  const endTime = reportprops[3].split("%3A").join(":");
+  const driverCNI = reportprops[4].split("%22")[0];
+
+  const Today = new Date(Date()).toLocaleDateString()
+
+  //-------------------------------------------api calling for getting data
+  const getTripOfDriver = async () => {
+    const response = await fetch(
+      `/api/generalReports/tripOfDriver/'${startDate}'/'${endDate}'/'${startTime}'/'${endTime}'/'${driverCNI}'`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const result = await response.json();
+    setData(result);
+  };
+
+  //------------------------------------------------------excel headers
+  const header = [
+    "Sector",
+    "Location",
+    "Time",
+    "vehicle No",
+    "Expiry",
+    "Checked By",
+  ]; //table heads
+  //-----------getting data on page load
+
+  useEffect(() => {
+    getTripOfDriver();
+    console.log(Today)
+
+  
+  },[]);
+
+  return (
+    <div>
+      <ReportTemp
+        reportName="PSVs Travelled"
+        header={header}
+        data={data}
+        starDate={startDate}
+        endDate={endDate}
+      />
+
+      <div className="w-full flex gap-5 flex-col p-4 justify-center items-center ">
+        <div className = "flex flex-col w-3/5 gap-3 justify-center items-center">
+
+            <div className=" font-extrabold text-lg bg-teal-600 p-3 rounded-md">{data[0].DriverName}</div>
+            <div className="w-3/4 flex flex-row gap-3"> <div className="w-1/6 font-bold">License No</div><div>{data[0].DriverLicenceNo}</div></div>
+            <div className="flex flex-row w-full justify-between items-end px-5">
+            <div className="w-3/4 flex flex-row gap-3"> <div className="w-1/6 font-bold">Date</div><div>{` From: ${startDate} To ${endDate}`}</div></div>
+            <div className="w-3/4 flex flex-row gap-3"> <div className="w-1/6 font-bold">Time</div><div>{` From: ${startTime} To ${endTime}`}</div></div>
+            
+            </div>
+      </div>
+      </div>
+
+      {/* ======================================Report Data Here */}
+
+      <table className="flex justify-center items-center text-start">
+        <tbody>
+          <tr>
+            {header.map((head) => (
+              <td key={head} className="bg-gray-400  font-bold  p-2 border border-black">
+                {head}
+              </td>
+            ))}
+          </tr>
+            {data.map((item)=>(
+              <tr className ="even:bg-white odd:bg-slate-300">
+                 <td className =' p-2 border border-black'>{item.Sector}</td>
+                 <td className =' p-2 border border-black'>{`${item.KM}  ${item.Side}`}</td>
+                 <td className =' p-2 border border-black'>{item.Time}</td>
+                 <td className =' p-2 border border-black'>{item.RegNo}</td>
+                 <td className ='p-2 border border-black'>{item.LicenseExpiry.split('T')[0]>Today?"Valid":"Expired"}</td>
+                 {/* <td className =' p-2 border border-black'>{item.RouteExpiryDate.split('T')[0].split('-').reverse().join('-')}</td> */}
+                 {/* <td className =' p-2 border border-black'>{item.FitnessExpiryDate.split('T')[0].split('-').reverse().join('-')}</td> */}
+                 {/* <td className ='bg-gray-200 p-2 border border-black'>{item.TypeExpiryDate?.split('T')[0].split('-').reverse().join('-')}</td> */}
+                 <td className ='p-2 border border-black'>{item.FullName}</td>
+          </tr>  
+            )  
+            )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ pages/api/generalReports/tripOfDriver/[...tripOfDriverApi].js
@@ -0,0 +1,16 @@
+import db from '../../../../db'
+
+
+
+export default async function tripOfDrivers(req, res) {
+  if (req.method === 'GET') {
+    const {tripOfDriverApi} = req.query
+    
+    const qry=`select Distinct d.Sector,b.KM,b.Side, cast(cast(b.AddedON as time) as varchar(5)) [Time],b.RegNo,c.DriverName ,b.DriverLicenceNo,b.LicenseExpiry, d.FullName from  SearchData as b  inner join  DriverInfo as c on b.CNIC = c.CNIC  inner join COMMON_USER d on b.AddedBy = d.UserName where   b.AddedON >${tripOfDriverApi[0]} and b.AddedON <${tripOfDriverApi[1]} and cast(b.AddedON as time)>${tripOfDriverApi[2]}and cast(b.AddedON as time)<${tripOfDriverApi[3]} and b.CNIC = ${tripOfDriverApi[4]}`
+    const result = await db.query(qry)
+   
+    res.status(200).json(result.recordset)
+  } 
+   
+  }
+
