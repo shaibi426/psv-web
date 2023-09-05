8e57c19aa8250e622b7d26d476da5e22909dd835
m-atique <atiquenwl@gmail.com>
2023-09-05 20:23:50 +0500

    udated main page

--- app/components/generalReports/PSvsTravelled/[...reportData]/page.tsx
+++ app/components/generalReports/PSvsTravelled/[...reportData]/page.tsx
@@ -39,7 +39,7 @@ export default  function PSVsTravelled(props:any) {
     
   };
   getData()
- },[])
+ },[startDate,endDate,startTime,endTime,psvNo])
  
 
   //------------------------------------------------------excel headers
