132c651bd0e5554cb36fa3a4beba96f2d292366f
m-atique <atiquenwl@gmail.com>
2023-09-05 21:44:11 +0500

    home page responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import MainBck from '../public/2.jpg'
 import logo from './components/assests/PMP LOGO.png'
 import React ,{ useEffect,useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { User2,KeyRound } from 'lucide-react'
 
 
 
@@ -64,36 +65,42 @@ const router = useRouter()
   return (
     <div className="bg-[#051532]">
       <div className=' w-full flex flex-row  gap-2 items-center justify-stretch pl-10 pt-5'>
-      <Image src={logo}  alt="logo" width={80} height ={80} 
+      <Image src={logo}  alt="logo" width={80} height ={80} className='sm:w-30 sm:h-30 w-16 h-16'
        />
-       <h1 className=' font-extrabold text-3xl text-white font-prompt '>PSVs Management Information System (PSV-MIS)</h1>
+       <h1 className=' font-extrabold sm:text-3xl text-white font-prompt '> <span className='hidden sm:block'>PSVs Management Information System</span> (PSV-MIS)</h1>
         </div>
-    <div className=" h-screen flex justify-between items-center">
-      <div className ='flex items-start pt-10 h-full w-2/4 justify-center'>
+    <div className=" h-screen flex justify-between items-center ">
+      <div className =' items-start pt-10 h-full w-2/4 justify-center hidden sm:block'>
       <Image src={MainBck}  alt="main page background" className=''
        />
        </div>
-       <div className ="w-2/5 h-full pb-10 flex items-start ">
-        <div className='bg-blue-100 w-3/4 bg-opacity-10 rounded-md h-3/4 pt-15 p-10 flex flex-col justify-center gap-3'>
+       <div className ="sm:w-2/5 h-full pb-10 flex items-start justify-center w-full ">
+        <div className='bg-blue-100 sm:w-3/4  bg-opacity-10 rounded-md h-3/4 pt-15 p-10 flex flex-col justify-center gap-3'>
 
           <div>
             <h1 className='text-3xl pb-5 font-extrabold text-yellow-400'> Log <span className='text-blue-400'> in</span> </h1>
           </div>
          <p className ={`text-xs text-red-600 ${emptyUser}`}>Please Enter User ID</p>
+         <div className='flex-row flex items-center  bg-white'> 
+         <User2 stroke='white' className=' absolute  bg-blue-500 w-[32px] h-[40px] p-2' />
         <input 
-        className='rounded-sm p-2 text w-full'
+        className='rounded-sm p-2 text w-full pl-10'
         placeholder='User'
         type ='text'
         value ={user}
         onChange={(e)=>setUser(e.target.value)}
          />
+         </div>
+         <div className='flex-row flex items-center  bg-white'> 
+         <KeyRound stroke='white' className='absolute  bg-blue-500 w-[32px] h-[40px] p-2' />
         <input 
-        className='rounded-sm p-2 text w-full' 
+        className='rounded-sm p-2 text w-full pl-10' 
         placeholder='Passward' 
         type='password'
         value ={pwd}
         onChange={(e)=>setPwd(e.target.value)}
         />
+         </div>
          <p className ={`text-xs text-red-600 ${emptyPwd} `}>Please Enter Password</p>
         <div className='flex w-full justify-end pt-10'>
         <button
