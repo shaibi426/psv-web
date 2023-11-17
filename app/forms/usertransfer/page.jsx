ed3444eaf5a74e03bb9e93d3a6d76c249261340e
m-atique <atiquenwl@gmail.com>
2023-11-17 14:56:44 +0500

    user transfer from updated

--- app/forms/usertransfer/page.jsx
+++ app/forms/usertransfer/page.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Usertransfer = () => {
   return (
-    <div>
-      <div></div>
+    <div className="bg-white flex flex-row justify-center w-full">
+      <div className="bg-white overflow-hidden w-[1440px] h-[1024px] relative">
+        <div className="absolute w-[653px] h-[625px] top-[6px] left-[-346px]">
+          <div className="absolute w-[379px] h-[347px] top-[83px] left-[67px] bg-[#bd060654] rounded-[62px] rotate-[45.75deg]" />
+          <div className="absolute w-[208px] h-[226px] top-[359px] left-[395px] bg-[#b2ccfde6] rounded-[32px] rotate-[45.75deg]" />
+        </div>
+        <img className="absolute w-[181px] h-[250px] top-[299px] left-[1259px]" alt="Rectangle" src="rectangle-3.svg" />
+        <div className="absolute w-[766px] h-[848px] top-[96px] left-[320px] rounded-[20px] border border-solid border-[#ab9f9f] [background:linear-gradient(180deg,rgba(177.95,176.37,255,0.38)_17.19%,rgba(249.69,194.55,194.55,0.35)_100%)]">
+          <div className="absolute w-[438px] h-[68px] top-[42px] left-[163px] [font-family:'Inter-ExtraBold',Helvetica] font-extrabold text-black text-[32px] text-center tracking-[0] leading-[normal]">
+            OFFICER TRANFER
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Usertransfer
